bceb5ca4041608bda2787a2a00dd642a5bb6ed2d
Will Adams <bushblade@gmail.com>
2019-06-04 16:31:49 +0100

    working movement including diagonal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+// import styled from 'styled-components'
 import logo from './logo.svg'
 
 function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const inputRef = useRef()
+
+  const [keypress, setKeyPress] = useState({
+    ArrowDown: false,
+    ArrowUp: false,
+    ArrowLeft: false,
+    ArrowRight: false
+  })
+
+  const handleKeyDown = e => {
+    if (!keypress[e.key]) {
+      setKeyPress({ ...keypress, [e.key]: true })
+    }
+  }
+  const handleKeyUp = e => {
+    setKeyPress({ ...keypress, [e.key]: false })
+  }
+  useEffect(() => {
+    inputRef.current.focus()
+    let interval = setInterval(() => {
+      if (Object.values(keypress).some(val => val)) {
+        const { ArrowDown, ArrowUp, ArrowLeft, ArrowRight } = keypress
+        let newPosition = { ...position }
+        if (ArrowDown) newPosition.y = position.y + 5
+        if (ArrowUp) newPosition.y = position.y - 5
+        if (ArrowLeft) newPosition.x = position.x - 5
+        if (ArrowRight) newPosition.x = position.x + 5
+        setPosition(newPosition)
+      }
+    }, 10)
+    return () => clearInterval(interval)
+  }, [position, keypress])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        style={{ height: 0, width: 0, opacity: 0 }}
+        ref={inputRef}
+        onBlur={() => inputRef.current.focus()}
+        onKeyDown={handleKeyDown}
+        onKeyUp={handleKeyUp}
+      />
+      <div>
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          style={{
+            width: '6rem',
+            height: '6rem',
+            transform: `translate3d(${position.x}px,${position.y}px,0)`
+          }}
+        />
+      </div>
     </div>
   )
 }
